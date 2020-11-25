1e8f0a650e83e259d6eea605b891902b8d514e32
stebssbets <stebssbets@yahoo.com>
2020-11-25 10:41:28 +0700

    waypoint boxes for prototype placed and detecting intention of picking up or delivering

---
+++ Assets/WaypointScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointScript : MonoBehaviour
+{
+    public bool listeningForPlayerSpeed = false;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("scooter");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            listeningForPlayerSpeed = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (listeningForPlayerSpeed)
+        {
+            if (player.GetComponent<ScooterDrive>().currentSpeed == 0)
+            {
+                Debug.Log("delivery or drop off detected");
+                listeningForPlayerSpeed = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
