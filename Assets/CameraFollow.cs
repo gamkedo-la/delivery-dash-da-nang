5724ec2c80aac883a8bdf6fc5b03a03588003a9f
Cassidy Noble <cub.noble@yahoo.fr>
2020-12-01 18:53:45 -0600

    Adjusting Canvas Text. Creating Camera Follow.
    Adjusting the Canvas text to make it more legible.
    Creating Camera follow and target Camera points.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offsetBehindBike;
+
+    public Transform lookAtTarget;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offsetBehindBike;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        this.gameObject.transform.LookAt(lookAtTarget);
+    }
+
+}
