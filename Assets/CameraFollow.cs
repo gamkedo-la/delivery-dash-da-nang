7325b32d9455fe19220ceabe96c79b618cf34c05
Cassidy Noble <cub.noble@yahoo.fr>
2020-12-05 07:40:21 -0600

    Multiple Player Perspectives (KeyCode 1)
    Ability to change camera perspectices

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public float smoothSpeed = 0.125f;
-    public Vector3 offsetBehindBike;
+    public GameObject[] target;
+    public float[] smoothSpeed;
 
-    public Transform lookAtTarget;
+    public GameObject[] lookAtTarget;
+
+    int viewWindowCount;
 
     private void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offsetBehindBike;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        Vector3 desiredPosition = target[viewWindowCount].transform.position;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed[viewWindowCount]);
         transform.position = smoothedPosition;
 
-        this.gameObject.transform.LookAt(lookAtTarget);
+        this.gameObject.transform.LookAt(lookAtTarget[viewWindowCount].transform.position);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (viewWindowCount >= target.Length-1)
+            {
+                viewWindowCount = 0;
+            }
+            else
+            {
+                viewWindowCount++;
+            }
+        }
     }
 
 }
