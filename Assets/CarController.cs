91200c8110ce12c2b2a3f1187510740e03fe5130
Cassidy Noble <cub.noble@yahoo.fr>
2020-12-05 15:56:59 -0600

    Created Test Scene. Created new driving controller in Test Scene to help figure out drifting.
    Don't plan on using new driving controller.

---
+++ Assets/CarController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardAccel = 8, reverseAccel = 4, maxSpeed = 50, turnStrength = 180;
+
+    private float speedInput, turnInput;
+
+    float gravityForce = 10f;
+
+    private bool grounded;
+    public float dragGround;
+
+    public LayerMask whatIsGround;
+    public float groundRayLength = 0.5f;
+    public Transform groundRayPoint;
+
+    private void Start()
+    {
+        rb.transform.parent = null;
+    }
+
+    private void Update()
+    {
+        speedInput = 0;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
+        }
+
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel * 1000f;
+        }
+
+        turnInput = Input.GetAxis("Horizontal");
+
+        if (grounded)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0));
+        }
+
+        transform.position = rb.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = false;
+        RaycastHit hit;
+
+        if (Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
+        {
+            grounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+        if (grounded)
+        {
+            rb.drag = dragGround;
+
+            if (Mathf.Abs(speedInput) > 0)
+            {
+                rb.AddForce(transform.forward * speedInput);
+            }
+        }
+
+        else
+        {
+            rb.drag = 0.1f;
+
+            rb.AddForce(Vector3.up * -gravityForce * 100);
+        }
+    }
+}
