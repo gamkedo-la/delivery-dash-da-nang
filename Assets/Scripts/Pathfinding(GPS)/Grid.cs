fad31f79a2a2344defefeba02cec247d03770a8f
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2020-12-10 13:58:37 -0500

    Setting up GPS Pathfinding (WIP)

---
+++ Assets/Scripts/Pathfinding(GPS)/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public LayerMask undriveableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                // TODO Continue script tutorial here at 11:12
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+    }
+}
---
+++ Assets/Scripts/Pathfinding(GPS)/Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Pathfinding based on Sebasian Lague's A* Pathfinding series
+
+public class Node : MonoBehaviour
+{
+
+    public bool driveable;
+    public Vector3 worldPosition;
+
+    public Node(bool _driveable, Vector3 _worldPos)
+    {
+        driveable = _driveable;
+        worldPosition = _worldPos;
+    }
+
+}
