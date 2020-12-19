0a5daa7d1964e78f77cc3b08768a3b3b5c7b93ea
Cassidy Noble <cub.noble@yahoo.fr>
2020-12-19 08:54:02 -0600

    Working on the order prefab for the phone app
    can accept and cancel order, no prefab, no functionality at this time

--- Assets/OrderSystem.cs
+++ Assets/OrderSystem.cs
@@ -9,5 +9,19 @@ public class OrderSystem : MonoBehaviour
      * 3. remove order from the list
      * 4. add order to the player UI
      * 5. list can be added to at random and orders can be removed at random (if not selected)
+     * 6. Ability to cancel order once accepted
      * */
+
+    public string[] restaurantName;
+    public string[] productName;
+
+    public Transform[] customerLocations;
+    public string[] apartmentName;
+    public string[] customerNames;
+
+    public int restaurantSelected;
+    public int customerLocation;
+    public int customerName;
+
+
 }
---
+++ Assets/PrefabOrder.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrefabOrder : MonoBehaviour
+{
+    public string[] restaurantName;
+    public string[] apartmentName;
+    public string[] customerNames;
+
+    public int restaurantSelected;
+    public int customerLocation;
+    public int customerName;
+
+    public Text orderText;
+    public Text orderState;
+
+    bool orderHasBeenTaken;
+    public Image orderCondition;
+
+    public GameObject accept, decline;
+
+    private void Start()
+    {
+        restaurantSelected = Random.Range(0, restaurantName.Length);
+        customerName = Random.Range(0, customerNames.Length);
+        customerLocation = Random.Range(0, apartmentName.Length);
+
+        orderText.text = customerNames[customerName].ToString() + " ordered from " + restaurantName[restaurantSelected].ToString() + " to deliver to " + apartmentName[customerLocation].ToString();
+    }
+
+    public void OrderAccepted()
+    {
+        orderHasBeenTaken = true;
+        accept.SetActive(false);
+        decline.SetActive(true);
+        orderCondition.color = Color.green;
+        //display ui order for the player
+    }
+
+    public void OrderDeclined()
+    {
+        if (orderHasBeenTaken)
+        {
+            orderHasBeenTaken = false;
+            accept.SetActive(true);
+            decline.SetActive(false);
+            orderCondition.color = Color.yellow;
+        }
+    }
+}
