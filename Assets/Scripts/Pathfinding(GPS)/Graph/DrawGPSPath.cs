e3bba7eec3926c7b0fe66c2e213c00455a1b6c3e
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-12-27 16:59:54 -0500

    GPS Will draw updated path lines on minimap, follows player

---
+++ Assets/Scripts/Pathfinding(GPS)/Graph/DrawGPSPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGPSPath : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+
+    GraphPathfinding graphPathfinding;
+
+
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        graphPathfinding = FindObjectOfType<GraphPathfinding>();
+    }
+
+    private void Update()
+    {
+        List<GraphNode> pathNodes = graphPathfinding.path;
+        var points = new Vector3[pathNodes.Count];
+
+        for (int i = 0; i < pathNodes.Count; i++)
+        {
+            points[i] = pathNodes[i].worldPosition;
+        }
+        lineRenderer.positionCount = pathNodes.Count;
+        lineRenderer.SetPositions(points);
+    }
+}
