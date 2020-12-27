5bb0a8d8e10d83173836c32c31822010f5cbee8f
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-12-27 16:01:08 -0500

    GPS Simple Camera Follow Script

---
+++ Assets/Scripts/GPSCameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSCameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    [SerializeField]
+    private float cameraXRotation = 90f;
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(cameraXRotation, player.eulerAngles.y, 0f);
+    }
+}
