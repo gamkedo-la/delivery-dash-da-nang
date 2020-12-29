d01c8938c3334ff6dd82392e4b0c6a83eb7ced2d
Cassidy Noble <cub.noble@yahoo.fr>
2020-12-29 13:34:46 -0600

    GameOver Screen Created with Sorted Score List
    Game Over Screen with Sorted Players

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -19,14 +19,32 @@ public class GameManager : MonoBehaviour
     public static bool player1OrderDelivered;
 
     public static float player1ScoreOnOrder;
-    public static float player1ScoreTotal;
+    public static float player1ScoreTotal, player2ScoreTotal, player3ScoreTotal, player4ScoreTotal;
+
+    public List<float> playerScores = new List<float>(); 
+
+    public Text player1ScoreText, player2ScoreText, player3ScoreText, player4ScoreText;
 
     //This is the macro game timer
     public float TimeRemaining;
     public Text TimeUI;
+    public GameObject RoundOverObject;
 
     private void Start()
     {
+
+        //This is for testing, remove later
+        player1ScoreTotal = Random.Range(0, 100);
+        player2ScoreTotal = Random.Range(0, 100);
+        player3ScoreTotal = Random.Range(0, 100);
+        player4ScoreTotal = Random.Range(0, 100);
+
+        playerScores.Add(player1ScoreTotal);
+        playerScores.Add(player2ScoreTotal);
+        playerScores.Add(player3ScoreTotal);
+        playerScores.Add(player4ScoreTotal);
+
+
         player1OrderPickedUp = false;
         player1OrderDelivered = false;
     }
@@ -44,11 +62,25 @@ public class GameManager : MonoBehaviour
 
     void TimeRemainingInRound()
     {
-        TimeRemaining -= Time.deltaTime;
+        if (TimeRemaining >= 0)
+        {
+            TimeRemaining -= Time.deltaTime;
 
-        float minutes = Mathf.FloorToInt(TimeRemaining / 60);
-        float seconds = Mathf.FloorToInt(TimeRemaining % 60);
+            float minutes = Mathf.FloorToInt(TimeRemaining / 60);
+            float seconds = Mathf.FloorToInt(TimeRemaining % 60);
 
-        TimeUI.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            TimeUI.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+
+        else
+        {
+            RoundOverObject.SetActive(true);
+            playerScores.Sort();
+            playerScores.Reverse();
+            player1ScoreText.text = playerScores[0].ToString("F0");
+            player2ScoreText.text = playerScores[1].ToString("F0");
+            player3ScoreText.text = playerScores[2].ToString("F0");
+            player4ScoreText.text = playerScores[3].ToString("F0");
+        }
     }
 }
