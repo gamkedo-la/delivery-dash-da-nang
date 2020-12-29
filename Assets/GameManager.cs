da04f34c09801a1d0abf7f4f3d95cc9ec0a6ed1c
Cassidy Noble <cub.noble@yahoo.fr>
2020-12-29 12:53:57 -0600

    Countdown Timer
    Countdown Timer Implemented

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -21,6 +21,10 @@ public class GameManager : MonoBehaviour
     public static float player1ScoreOnOrder;
     public static float player1ScoreTotal;
 
+    //This is the macro game timer
+    public float TimeRemaining;
+    public Text TimeUI;
+
     private void Start()
     {
         player1OrderPickedUp = false;
@@ -34,5 +38,17 @@ public class GameManager : MonoBehaviour
             player1RestaurantWayPoint.transform.position = Player1RestaurantWayPoint.transform.position;
             player1ApartmentWayPoint.transform.position = Player1ApartmentWayPoint.transform.position;
         }
+
+        TimeRemainingInRound();
+    }
+
+    void TimeRemainingInRound()
+    {
+        TimeRemaining -= Time.deltaTime;
+
+        float minutes = Mathf.FloorToInt(TimeRemaining / 60);
+        float seconds = Mathf.FloorToInt(TimeRemaining % 60);
+
+        TimeUI.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
