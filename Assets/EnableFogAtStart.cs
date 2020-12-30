6f089cad197626c2e382150a32eca3480ff3e08f
stebssbets <stebssbets@yahoo.com>
2020-12-30 09:34:51 +0700

    fog disabled for editor window. Scripted back on start of game, script on camera

---
+++ Assets/EnableFogAtStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableFogAtStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.fog = true;
+        Debug.Log("Script turning on fog so it's not obstructing editor view");
+    }
+}
