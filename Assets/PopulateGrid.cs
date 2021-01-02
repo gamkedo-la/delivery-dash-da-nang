8dac353aa60163d8411c2b30d4387c1f77778897
Cassidy Noble <cub.noble@yahoo.fr>
2021-01-02 09:00:26 -0600

    Starting the Grid Layout Process for Orders

---
+++ Assets/PopulateGrid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateGrid : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public int numberToCreate;
+
+    private void Start()
+    {
+        Populate();
+    }
+
+    void Populate()
+    {
+        GameObject newObj;
+        for (int i = 0; i < numberToCreate; i++)
+        {
+            newObj = (GameObject)Instantiate(prefab, transform);
+           // newObj.GetComponent<Image>().color = Random.ColorHSV();
+        }
+    }
+}
