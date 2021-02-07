d92e807e60beb27ef10d8f3e778112b3229ad411
Christer Kaitila <breakdance@mcfunkypants.com>
2021-02-06 16:23:27 -0800

    on screen FPS display to help us with perf issues

---
+++ Assets/FpsDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+ 
+public class FpsDisplay : MonoBehaviour
+{
+    public Text fpsText;
+    public float hudRefreshRate = 1f;
+ 
+    private float timer;
+ 
+    private void Update()
+    {
+        if (Time.unscaledTime > timer)
+        {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            fpsText.text = fps + " FPS";
+            timer = Time.unscaledTime + hudRefreshRate;
+        }
+    }
+}
