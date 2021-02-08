c01c71e2d55cf9a2f57afe26d2b98f86a09110da
stebssbets <stebssbets@yahoo.com>
2021-02-08 14:20:18 +0700

    WIP: Starting a parked car generator

---
+++ Assets/ParkedCarSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkedCarSpawner : MonoBehaviour
+{
+    public GameObject parkedCar1;
+
+    public float parkedCarXSpread;
+    public float parkedCarYSpread;
+    public float parkedCarZSpread;
+
+    void SpreadCars()
+    {
+        /*Vector3 RandomPosition = new Vector3(Random.Range(-parkedCarXSpread, parkedCarXSpread),
+                                            (Random.Range(-parkedCarYSpread, parkedCarYSpread),
+                                            (Random.Range(-parkedCarZSpread, parkedCarZSpread))
+
+        GameObject clone = Instantiate(parkedCar1, RandomPosition, Quaternion.identity);*/
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
