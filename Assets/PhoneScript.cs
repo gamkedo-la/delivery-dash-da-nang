460e10b8f5dd943d57d61458d9f1a36544582f3a
stebssbets <stebssbets@yahoo.com>
2021-02-17 21:09:40 +0700

    button clicking sound on phone hooked up

--- Assets/PhoneScript.cs
+++ Assets/PhoneScript.cs
@@ -10,57 +10,57 @@ public class PhoneScript : MonoBehaviour
 
     public void OrdersButtonPressed()
     {
-		AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject);
+		AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject, volume: 0.5f);
         HomeScreen.SetActive(false);
         OrdersMenu.SetActive(true);
     }
 
     public void OrdersBack() 
 	{
-		AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject);
-		HomeScreen.SetActive(true);
+        AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject, volume: 0.5f);
+        HomeScreen.SetActive(true);
         OrdersMenu.SetActive(false);
     }
 
     public void RatingsButtonPressed() 
 	{
-		AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject);
-		HomeScreen.SetActive(false);
+        AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject, volume: 0.5f);
+        HomeScreen.SetActive(false);
         RatingsScreen.SetActive(true);
     }
 
     public void RatingsBack() 
 	{
-		AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject);
-		HomeScreen.SetActive(true);
+        AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject, volume: 0.5f);
+        HomeScreen.SetActive(true);
         RatingsScreen.SetActive(false);
     }
 
     public void GPSButtonPressed() 
 	{
-		AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject);
-		HomeScreen.SetActive(false);
+        AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject, volume: 0.5f);
+        HomeScreen.SetActive(false);
         GPSMenu.SetActive(true);
     }
 
     public void GPSButtonBack() 
 	{
-		AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject);
-		GPSMenu.SetActive(false);
+        AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject, volume: 0.5f);
+        GPSMenu.SetActive(false);
         HomeScreen.SetActive(true);
     }
 
     public void CustomerScoresPressed() 
 	{
-		AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject);
-		HomeScreen.SetActive(false);
+        AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject, volume: 0.5f);
+        HomeScreen.SetActive(false);
         CurrentScores.SetActive(true);
     }
 
     public void CustomerScoresBack() 
 	{
-		AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject);
-		HomeScreen.SetActive(true);
+        AudioManager.Instance.PlaySoundSFX(phoneButtonPressedAudioClip, gameObject, volume: 0.5f);
+        HomeScreen.SetActive(true);
         CurrentScores.SetActive(false);
     }
 }
