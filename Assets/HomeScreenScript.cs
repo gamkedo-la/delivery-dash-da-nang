75c887e9ee2aa4a87f3e79c42f8cd1cebe54014b
Stebs <stebssbets@yahoo.com>
2021-03-01 19:53:53 +0700

    increased field of view on GPS minimap camera

--- Assets/HomeScreenScript.cs
+++ Assets/HomeScreenScript.cs
@@ -6,11 +6,11 @@ public class HomeScreenScript : MonoBehaviour
 {
 
     public List<GameObject> listOfButtons = new List<GameObject>();
-    public gameObject OrdersButton;
-    public gameObject GPSButton;
-    public gameObject AlertsButton;
-    public gameObject TBDButton;
-    public gameObject CurrentRatingButton;
+    public GameObject OrdersButton;
+    public GameObject GPSButton;
+    public GameObject AlertsButton;
+    public GameObject TBDButton;
+    public GameObject CurrentRatingButton;
 
     private int currentActiveButtonIndex = 0;
 
@@ -23,7 +23,7 @@ public class HomeScreenScript : MonoBehaviour
     void Start()
     {
 
-        listOfButtons.Add(GameO)
+        //listOfButtons.Add(GameO)
     }
 
     // Update is called once per frame
