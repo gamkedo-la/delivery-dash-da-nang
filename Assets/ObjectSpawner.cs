d630420cbcdb6d72c0d7f075ca4d974d85ed229c
Stebs <stebssbets@yahoo.com>
2021-03-13 10:05:07 +0700

    relabeling/refactoring object spawners and objects in the hierarchy/scene to help me think through refactoring/recoding waypoints and navigation programming

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject notThisParentPrefab;
+    public int objectsToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+
+        
+
+    int count = 0;
+        while (count<objectsToSpawn)
+        {
+            if (gameObject.name == "CarSpawner")
+            {
+                Debug.Log("inside while loop of car spawner");
+            }
+            GameObject notThisParentPrefabWithTransformOfManyVehicularChildren = Instantiate(notThisParentPrefab);
+            Transform waypointChildFromThisTransform = transform.GetChild(Random.Range(0, transform.childCount - 1));
+            Debug.Log(waypointChildFromThisTransform);
+            waypointChildFromThisTransform.GetComponent<WaypointNavigation>().currentWaypoint = waypointChildFromThisTransform.GetComponent<Waypoint>();
+            //Debug.Log(obj.GetComponent<WaypointNavigation>().currentWaypoint);
+            notThisParentPrefabWithTransformOfManyVehicularChildren.transform.position = waypointChildFromThisTransform.position;
+
+            yield return new WaitForEndOfFrame();
+
+            count++;
+        }
+    }
+}
