aa13c772da3e43a02c4cc8a9e8db58caf5f96500
Christer Kaitila <breakdance@mcfunkypants.com>
2021-03-23 09:16:27 -0700

    tons of new parked cars added to east-west streets

--- Assets/ParkedCarSpawnerNorthSouth.cs
+++ Assets/ParkedCarSpawnerNorthSouth.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ParkedCarSpawnerNorthSouth : MonoBehaviour
 {
+    public bool NorthSouth = true;
     public int minCarCount = 2;
     public int maxCarCount = 6;
     public float chancePerCar = 0.5f;
@@ -22,9 +23,16 @@ public class ParkedCarSpawnerNorthSouth : MonoBehaviour
             if (Random.value < chancePerCar) {
                 int picked = Random.Range(0,prefabs.Count);
                 //Debug.Log("Spawning parked car " + picked + " at dist " + dist);
+                
                 GameObject clone = Instantiate(prefabs[picked], new Vector3(0f,0f,0f), Quaternion.identity);
                 clone.transform.SetParent(transform); // be a child of this object
-                clone.transform.localPosition = new Vector3(0f,0f,dist+(carSpacing/2));
+                if (NorthSouth) {
+                    clone.transform.localPosition = new Vector3(0f,0f,dist+(carSpacing/2));
+                } else { // East West
+                    clone.transform.localPosition = new Vector3(dist+(carSpacing/2),0f,0f);
+                    clone.transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
+                }
+
             } 
             // otherwise we leave a parking space
 
@@ -35,11 +43,33 @@ public class ParkedCarSpawnerNorthSouth : MonoBehaviour
     }
     void OnDrawGizmos()//Selected()
     {
+        Vector3 cubePos;
+        Vector3 cubeSize;
+        
         // Draw a semitransparent cube at the transforms position
         Gizmos.color = new Color(1, 0, 0, 0.25f);
-        Vector3 cubePos = new Vector3(transform.position.x,transform.position.y+2.5f,
-            transform.position.z + maxCarCount * carSpacing / 2);
-        Gizmos.DrawCube(cubePos, new Vector3(10, 5, maxCarCount * carSpacing));
+
+        if (NorthSouth) {
+            
+            cubePos = new Vector3(
+                transform.position.x,
+                transform.position.y+2.5f,
+                transform.position.z + maxCarCount * carSpacing / 2);
+            
+            cubeSize = new Vector3(10, 5, maxCarCount * carSpacing);
+
+        } else { // East West
+
+            cubePos = new Vector3(
+                transform.position.x + maxCarCount * carSpacing / 2,
+                transform.position.y+2.5f,
+                transform.position.z);
+            
+            cubeSize = new Vector3(maxCarCount * carSpacing, 5, 10);
+
+        }
+
+        Gizmos.DrawCube(cubePos, cubeSize);
     }
     
 }
