daa9cef2705a780ee5594118fcd807b2d3415f7c
Stebs <stebssbets@yahoo.com>
2021-03-27 15:34:01 +0700

    cub with flatscreen prefab variant made. Several scattered across the city

---
+++ Assets/NPCScooterFlatScreenDrive.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCScooterFlatScreenDrive : MonoBehaviour
+{
+    public float flatScreenScooterSpeed = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 distanceToTranslate = new Vector3(0, 0, flatScreenScooterSpeed);
+        transform.Translate(distanceToTranslate);
+    }
+}
