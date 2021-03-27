95e4a0dc80b36a1162629a4173098f1d8d3c93ae
cubnoble <cub.noble@yahoo.fr>
2021-03-27 14:49:42 -0500

    Making the Level Loading screen more dynamic (so player doesn't worry the game froze)

---
+++ Assets/UpdatingText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdatingText : MonoBehaviour
+{
+    public Text textbox;
+    float currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  print(currentTime);
+        if (currentTime < 1)
+        {
+            currentTime += Time.deltaTime * 2;
+            if (currentTime > 0 && currentTime < 0.25f)
+            {
+                textbox.text = "Loading ";
+            }
+            if (currentTime > 0.25f && currentTime < 0.5f)
+            {
+                textbox.text = "Loading .";
+            }
+
+            if (currentTime > 0.5f && currentTime < 1)
+            {
+                textbox.text = "Loading . . ";
+            }
+        }
+
+        if (currentTime > 1 )
+        {
+            textbox.text = "Loading . . .";
+            currentTime = 0;
+        }
+    }
+}
