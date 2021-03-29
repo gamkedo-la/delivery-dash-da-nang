787a65afdf2f5fe9778b04b92f92b48e23f5b82c
Gamkedo <chris@gamkedo.com>
2021-03-28 21:21:06 -0700

    Replaced Unity default gradient skybox with flat blue, bypassed fog script at start so editor better matches how game shows deployed to webGL

--- Assets/EnableFogAtStart.cs
+++ Assets/EnableFogAtStart.cs
@@ -7,7 +7,7 @@ public class EnableFogAtStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        RenderSettings.fog = true;
+        // RenderSettings.fog = true;
         //Debug.Log("Script turning on fog so it's not obstructing editor view");
     }
 }
