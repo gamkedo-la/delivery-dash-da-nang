888d9c6fa07cc3d90f7d6e4e20d65022cf2435ae
Stebs <stebssbets@yahoo.com>
2021-03-29 17:46:26 +0700

    basic paperboy style unexpected scooter pulls out into road created

---
+++ Assets/PulloutTriggerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulloutTriggerScript : MonoBehaviour
+{
+    public GameObject parentScooter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            parentScooter.transform.transform.Rotate(0, 180, 0);
+            parentScooter.transform.GetComponent<NPCScooterFlatScreenDrive>().enabled = true;
+        }
+    }
+}
