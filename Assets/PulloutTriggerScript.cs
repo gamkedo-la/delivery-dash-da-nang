3619d2559a3c3254f3779dc77486c8b3f5993390
Stebs <stebssbets@yahoo.com>
2021-03-30 15:57:57 +0700

    attempted to stop pullout scooter from rotating 180 after pulling out... will readdress if it becomes a bigger problem

--- Assets/PulloutTriggerScript.cs
+++ Assets/PulloutTriggerScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PulloutTriggerScript : MonoBehaviour
 {
     public GameObject parentScooter;
-    private BoxCollider boxCollider;
+    private BoxCollider thisBoxCollider;
+    public GameObject otherParentTriggerObject;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,14 +14,17 @@ public class PulloutTriggerScript : MonoBehaviour
         {
             parentScooter.transform.transform.Rotate(0, 180, 0);
             parentScooter.transform.GetComponent<NPCScooterFlatScreenDrive>().enabled = true;
+            otherParentTriggerObject.transform.GetComponent<BoxCollider>().isTrigger = false;
+            gameObject.transform.GetComponent<BoxCollider>().isTrigger = false;
         }
     }
 
     void OnDrawGizmos()
     {
-        boxCollider = transform.GetComponent<BoxCollider>();
+        thisBoxCollider = transform.GetComponent<BoxCollider>();
         // Draw a semitransparent blue cube at the transforms position
         Gizmos.color = new Color(1, 0, 0, 1f);
-        Gizmos.DrawCube(new Vector3(boxCollider.center.x, boxCollider.center.y, boxCollider.center.z), new Vector3(boxCollider.size.x, boxCollider.size.y, boxCollider.size.y));
+        Gizmos.DrawCube(new Vector3(gameObject.transform.GetComponent<BoxCollider>().center.x, gameObject.transform.GetComponent<BoxCollider>().center.y, gameObject.transform.GetComponent<BoxCollider>().center.z), 
+            new Vector3(gameObject.transform.GetComponent<BoxCollider>().size.x, gameObject.transform.GetComponent<BoxCollider>().size.y, gameObject.transform.GetComponent<BoxCollider>().size.y));
     }
 }
