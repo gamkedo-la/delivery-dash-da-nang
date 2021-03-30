434ea521272e82d9685031a26eda882f4faa1f01
Stebs <stebssbets@yahoo.com>
2021-03-30 14:51:52 +0700

    unexpected pullout cub recreated and test implementation successful with no FPS issue

--- Assets/PulloutTriggerScript.cs
+++ Assets/PulloutTriggerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PulloutTriggerScript : MonoBehaviour
 {
     public GameObject parentScooter;
+    private BoxCollider boxCollider;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,5 +14,13 @@ public class PulloutTriggerScript : MonoBehaviour
             parentScooter.transform.transform.Rotate(0, 180, 0);
             parentScooter.transform.GetComponent<NPCScooterFlatScreenDrive>().enabled = true;
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        boxCollider = transform.GetComponent<BoxCollider>();
+        // Draw a semitransparent blue cube at the transforms position
+        Gizmos.color = new Color(1, 0, 0, 1f);
+        Gizmos.DrawCube(new Vector3(boxCollider.center.x, boxCollider.center.y, boxCollider.center.z), new Vector3(boxCollider.size.x, boxCollider.size.y, boxCollider.size.y));
     }
 }
